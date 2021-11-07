1b622a80c707f603ebdacf284b1fde75f910319f
Sean Espindola <sean.espindola@outlook.com>
2021-11-07 12:51:44 -0300

    implementação home e register

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -31,14 +31,33 @@ export function Home() {
   async function loadData() {
     const dataKey = '@savepass:logins';
     // Get asyncStorage data, use setSearchListData and setData
+    const response = await AsyncStorage.getItem(dataKey);
+    if (response) {
+      const parsedData = JSON.parse(response);
+      setSearchListData(parsedData);
+      setData(parsedData);
+    }
   }
 
   function handleFilterLoginData() {
     // Filter results inside data, save with setSearchListData
+    const filteredData = searchListData.filter(data => {
+      const isValid = data.service_name
+        .toLowerCase()
+        .includes(searchText.toLowerCase());
+      if (isValid) {
+        return data;
+      }
+    });
+    setSearchListData(filteredData);
   }
 
   function handleChangeInputText(text: string) {
     // Update searchText value
+    if(!text) {
+      setSearchListData(data);
+    }
+    setSearchText(text);
   }
 
   useFocusEffect(useCallback(() => {
